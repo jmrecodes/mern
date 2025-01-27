23d3e49e0b913aba013f5de0b6a68db17160210f
jmrecodes <jmrecodes2022@gmail.com>
2025-01-28 07:08:51 +0800

    1.3

---
+++ src/server/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const server = express();
+
+server.use(express.static('dist'));
+
+server.set("view engine", "ejs");
+
+server.use("/", (req, res) => {
+    res.render("index", {
+        content: "EJS is <em>cool!"
+    });
+});
+
+server.listen("8080", "0.0.0.0", () => {
+    console.info(
+        "Express server is listening at http://0.0.0.0:8080"
+    );
+});
